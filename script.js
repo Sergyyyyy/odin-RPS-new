b9950530f4d35f6ec7df55e7346f5e8e1d4b9a6b
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-01-30 01:08:07 +0800

    Deleted old code, initialized variable ids, fixed swal2

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-/* -- PSUEDOCODE --
-
-getComputerChoice()
-    - return a string (rock, paper, scissors)
-    - use math.floor(math.random() * 3)
-
-getHumanChoice()
-    - prompt and get the value of the prompt
-    - return prompt value to console
-    - case-insensitive
-
-playRound()
-    - take parameters (humanChoice, computerChoice)
-    - log results
-    - increment scores
-
-playGame()
-    - run playRound() 5x with for loop
-    display score after running
-
-Global Scope
-    - choice array
-    - initialize humanScore and computerScore with the value of 0.
-
-- Assume that every input is correct.
-
-*/
-
 choices = ["rock", "paper", "scissors"]
 let humanScore = 0
 let computerScore = 0
@@ -37,56 +9,25 @@ function getComputerChoice() {
     return choices[randomChoice]
 }
 
-function getHumanChoice() {
-    let myChoice = prompt("Enter your choice:").toLowerCase()
+rock = document.getElementById("btn-rock")
+paper = document.getElementById("btn-paper")
+scissors = document.getElementById("btn-scissors")
 
-    console.log("You: " + myChoice)
-    return myChoice
-}
 
-function playRound(humanSelection, computerSelection) {
-    if (humanSelection == computerSelection) {
-        alert("Tie!")
-    }
-    else if (humanSelection == "rock" && computerSelection == "scissors") {
-        humanScore += 1
-        alert("You get a point.")
-    }
-    else if (humanSelection == "rock" && computerSelection == "paper") {
-        computerScore += 1
-        alert("Computer gets a point.")
-    }
-    else if (humanSelection == "scissors" && computerSelection == "rock") {
-        computerScore += 1
-        alert("Computer gets a point.")
-    }
-    else if (humanSelection == "scissors" && computerSelection == "paper") {
-        humanScore += 1
-        alert("You get a point.")
-    }
-    else if (humanSelection == "paper" && computerSelection == "scissors") {
-        computerScore += 1
-        alert("Computer gets a point.")
-    }
-    else if (humanSelection == "paper" && computerSelection == "rock") {
-        humanScore += 1
-        alert("You get a point.")
-    }
-}
 
-function decideWinner() {
-    let score;
+// testSwal = document.getElementById("btn-rock")
+// console.log(testSwal)
 
-    if (humanScore == computerScore) {
-        score = "The score is tied!"
-    }
-    else {
-        score = (humanScore > computerScore) ? `You win! Score: ${humanScore}` : `Computer wins! Score: ${computerScore}`
-    }
+// function newSwal(){
+//     Swal.fire({
+//         title: "How was your day?",
+//         confirmButtonText: "Good!",
+//         showDenyButton: true,
+//         denyButtonText: "Bad."
+//     })
+// }
 
-    console.log("\n" + score)
-    alert(score)
-}
+// testSwal.addEventListener("click", newSwal)
 
 
 
